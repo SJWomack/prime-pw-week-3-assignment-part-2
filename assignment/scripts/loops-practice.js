8153e65701bb1f04b72884ca21ecdbf1f3e24d8b
Stephen Womack <swomack111@gmail.com>
2022-04-17 15:06:59 -0500

    for loops task 1

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -6,7 +6,7 @@ console.log('---- 1. For loops ----');
 // Example: a for loop to console.log numbers from 0 to 3
 console.log('count from 0 to 3');
 // start i at 0, while i is < 4 do code between {}, afterwards add 1 to i (i++)
-for (let i=0; i<4; i++) {  
+for (let i = 0; i < 4; i++) {
   console.log(i);
 }
 
@@ -14,7 +14,7 @@ for (let i=0; i<4; i++) {
 //   - Which part of the example loop do you need to change to do this?
 console.log('count from 0 to 5');
 //need to change to i<6
-for (let i=0; i<6; i++) {  
+for (let i = 0; i < 6; i++) {
   console.log(i);
 }
 
@@ -22,14 +22,20 @@ for (let i=0; i<6; i++) {
 //   - Which part of the example loop do you need to change to do this?
 console.log('count from 3 to 5');
 //need to change i=3
-for (let i=3; i<6; i++) {  
+for (let i = 3; i < 6; i++) {
   console.log(i);
 }
 
 // 1.c. TODO: Write a for loop to console.log EVEN numbers from 2 to 10
 //   - Which part of the example loop do you need to change to do this?
 console.log('count even numbers from 2 to 10 (2, 4, 6, 8, 10):');
+//need to add conditional to the loop
 
+for (let i = 1; i < 11; i++) {
+  if (i % 2 === 0) {
+    console.log(i);
+  }
+}
 // 1.d. (STRETCH) TODO: Write a for loop to do a countdown from 5 to 0
 console.log('STRETCH: countdown from 5 to 0');
 
